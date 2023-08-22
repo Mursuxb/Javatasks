92c45f0a1837ba785e256467894f757717205668
Mursuxb <mursuxb@gmail.com>
2023-08-22 15:07:43 +0300

    Kesken

--- src/Week1_2/Task1.java
+++ src/Week1_2/Task1.java
@@ -8,10 +8,10 @@ public class Task1 {
         Scanner scanner = new Scanner(System.in);
 
         double celsius, fahrenheit;
-        System.out.println("Gimme Celsius");
-        celsius = scanner.nextDouble();
-        fahrenheit = 1.8*celsius+32;
-        System.out.printf("%.1f degrees celsius is %.1f degrees fahrenheit", celsius, fahrenheit);
+        System.out.println("Gimme Fahrenheit");
+        fahrenheit = scanner.nextDouble();
+        celsius = (fahrenheit-32)*5/9;
+        System.out.printf("%.1f degrees fahrenheit is %.1f degrees celsius", fahrenheit, celsius);
 
     }
 }
