95a11276e2b3be4ae375c9078492017f026d3a59
Mursuxb <mursuxb@gmail.com>
2023-08-22 13:07:21 +0300

    Initial commit

---
+++ src/Week1/Task3.java
@@ -0,0 +1,25 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The product of the numbers is " + (first * second * third));
+        float sum = (first + second + third);
+        System.out.println("The average of the numbers is " + (sum / 3));
+    }
+}
+
