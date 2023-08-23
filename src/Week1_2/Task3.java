b74ac9c8652ba0575eac644d295239799bdc1413
Mursuxb <mursuxb@gmail.com>
2023-08-23 12:41:21 +0300

    Done

--- src/Week1_2/Task3.java
+++ src/Week1_2/Task3.java
@@ -3,18 +3,27 @@ package Week1_2;
 import java.util.Scanner;
 
 public class Task3 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int leiviskä, naula;
-        double luoti;
-        System.out.println("Give lenght of first side");
-        side1 = scanner.nextDouble();
-        System.out.println("Give lenght of next side");
-        side2 = scanner.nextDouble();
+        /*
+        One leiviskä is 20 naula.
+        One naula is 32 luoti.
+        One luoti is 13.28 grams.
+        */
 
-        hypo = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
-        System.out.printf("The hypotenus is %.1f units long", hypo);
+        double luoti, grams, leiviskä, naula;
+
+        System.out.println("Give grams");
+        grams = scanner.nextDouble();
+        luoti = grams / 13.28;
+        naula = luoti / 32;
+        leiviskä = Math.floor(naula / 20);
+        naula = Math.floor(naula % 20);
+        luoti = luoti % 32;
+
+
+        System.out.printf("%.0f grams is %.0f leiviskä, %.0f naula, and %.2f luoti.", grams, leiviskä, naula, luoti);
     }
 }
 
